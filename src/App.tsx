1b2c0e238251e571cc4623d4e24d562f6f59530b
kanarskaya.e <kanarskaya.e@wildberries.work>
2024-12-19 14:57:40 +0300

    library solution

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { ReactTinderCards } from "./components/first-section/ReactTinderCards";
+
+function App() {
+  return (
+    <>
+      <ReactTinderCards />
+    </>
+  );
+}
+
+export default App;
---
+++ src/constants/constants.ts
@@ -0,0 +1,20 @@
+const imagesList = [
+  {
+    id: 0,
+    src: 'https://www.modi.ru/upload/product/b67/7h326lbewb0lkl1xaj8smruqhtv55zks.jpg',
+  },
+  {
+    id: 1,
+    src: 'https://www.modi.ru/upload/product/a0e/ikd6w9hzzp9r9jn9ygue5c8m1nn34spe.jpg',
+  },
+  {
+    id: 2,
+    src: 'https://www.modi.ru/upload/product/684/vm7ttbxjhfruja13olinfx5ojl450087.jpg',
+  },
+  {
+    id: 3,
+    src: 'https://www.modi.ru/upload/product/098/c20loz39nxow6e1i16tk9fnpu8b81ybw.jpg',
+  },
+]
+
+export default imagesList
